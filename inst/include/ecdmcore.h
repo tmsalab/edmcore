dbf328b4f2d5480d0fe3b31d4d4b87fd4ca64fd1
James Balamuta <balamut2@illinois.edu>
2019-06-19 17:42:14 -0500

    Begin implementation of header-only R package

---
+++ inst/include/ecdmcore.h
@@ -0,0 +1,7 @@
+#ifndef ECDMCORE_ECDMCORE_H
+#define ECDMCORE_ECDMCORE_H
+
+#include <RcppArmadillo.h>
+#include "q_matrix.h"
+
+#endif
---
+++ inst/include/q_matrix.h
@@ -0,0 +1,30 @@
+#ifndef ECDMCORE_Q_MATRIX_H
+#define ECDMCORE_Q_MATRIX_H
+
+namespace ecdmcore {
+
+inline bool check_identifiability(const arma::mat Q) {
+    unsigned int K = Q.n_cols;
+    unsigned int J = Q.n_rows;
+
+    arma::mat ones_zero_on_diag = -1 * arma::ones<arma::mat>(K, K);
+    arma::vec zeros_K = arma::zeros<arma::vec>(K);
+    ones_zero_on_diag.diag() = zeros_K;
+
+    arma::vec c_sum = (arma::sum(Q, 0)).t();
+    arma::vec r_sum = arma::sum(Q, 1);
+    arma::mat I_check = Q * ones_zero_on_diag;
+    arma::mat I_count = arma::zeros<arma::mat>(J, K);
+    I_count.elem(arma::find(I_check > -1)).fill(1.0);
+    arma::vec n_ek = (arma::sum(I_count, 0)).t();
+
+    double min_c = (arma::min(c_sum) > 2);
+    double min_r = (arma::min(r_sum) > 0);
+    double min_ek = (arma::min(n_ek) > 1);
+
+    return (min_c + min_r + min_ek > 2);
+}
+
+}
+
+#endif
