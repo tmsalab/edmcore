129d873e1aa6f486da4606c93861707abaf0bbaf
James Balamuta <balamut2@illinois.edu>
2019-06-19 10:32:41 -0500

    Deploy new usethis package-level roxygen2 structure

--- R/ecdmcore-package.R
+++ R/ecdmcore-package.R
@@ -1,4 +1,9 @@
-#' @useDynLib ecdmcore
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @useDynLib ecdmcore, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+#' @keywords internal
 "_PACKAGE"
-
