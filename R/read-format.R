cbb5f602814043089508d736b73e6851e508b1ff
James Balamuta <balamut2@illinois.edu>
2020-07-26 22:34:23 -0500

    Add helper functions for reading in Item/Q Matrix data.

---
+++ R/read-format.R
@@ -0,0 +1,57 @@
+#' Create a Generic Matrix Read Function
+#'
+#' Imports data from a flat file as a matrix with a predefined
+#' naming scheme.
+#'
+#' @return Provides a function that can be customized.
+#' @keywords internal
+#' @importFrom utils read.table
+#'
+#' @noRd
+read_psych = function(file, header = FALSE, sep = " ", skip = 0) {
+
+  # Read in data as a data.frame
+  read.table(file, header = header, sep = sep, skip = skip)
+
+}
+
+
+#' Import an Items Matrix
+#'
+#' Allows for a dichotomous items matrix to be imported with standard styling.
+#'
+#' @param file         name, url, or [connections] of the file to be read
+#' @param header       a logical value to indicate if the data contains a
+#'                     description field as the first row. Default is `FALSE`.
+#' @param sep          the field separator character. Values on each line of the
+#'                     file are separated by this character. Default is white
+#'                     space given by `sep = " "`.
+#' @param skip         the number of lines of the data file that should be
+#'                     skipped before the data is read. Default is 0.
+#'
+#' @return
+#' A `matrix` labeled with row names as `SubjectXX` and column names as `ItemYY`
+#'
+#' @export
+#' @details
+#' This function is designed to specifically read in dichotomous item matrices
+#' into _R_. The matrix must be structured with the appropriate separator.
+read_item_matrix = function(file, header = FALSE, sep = " ", skip = 0) {
+  read_psych(file, header = header, sep = sep, skip = skip)
+}
+
+#' Import a Q Matrix
+#'
+#' Allows for a dichotomous Q Matrix to be imported with standard styling.
+#'
+#' @inheritParams read_item_matrix
+#' @return
+#' A `matrix` labeled with row names as `ItemYY` and column names as `SkillZZ`
+#'
+#' @export
+#' @details
+#' This function is designed to specifically read in dichotomous Q matrix
+#' into _R_. The matrix must be structured with the appropriate separator.
+read_q_matrix = function(file, header = FALSE, sep = " ", skip = 0) {
+  as_q_matrix(read_psych(file, header = header, sep = sep, skip = skip))
+}
