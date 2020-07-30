91cd44401aa48039c7287b051f188752f8a31c5c
James Balamuta <balamut2@illinois.edu>
2020-07-30 01:12:20 -0500

    Clean up the mode metric

--- R/metrics.R
+++ R/metrics.R
@@ -1,3 +1,5 @@
+## Integer approaches -----
+
 #' Metric for determining the most popular value
 #'
 #' Computes the mode given a vector of data.
@@ -9,6 +11,15 @@
 #' @return
 #' A single `numeric` or `integer` value.
 #'
+#' @section Recovery Use:
+#' The mode should be used when computing latent class recovery among subjects.
+#'
+#' @details
+#' Be forewarned that this method of obtaining the mode does not take into
+#' consideration ties. That is, only the first group with the maximum value
+#' is returned. If a second group also has a similar count, this group will
+#' not be returned.
+#'
 #' @export
 #' @examples
 #' # Sample class-data
@@ -16,9 +27,15 @@
 #' metric_mode(x)
 metric_mode = function(x, na.rm = FALSE) {
 
+  # Remove missing values
   if (na.rm) {
     x = x[!is.na(x)]
   }
+
+  # Obtain unique values
   uniq_x = unique(x)
+
+  # Obtain the first occurrence of a maximum value
+  # Note: There are no tie-breakers here.
   uniq_x[which.max(tabulate(match(x, uniq_x)))]
 }
