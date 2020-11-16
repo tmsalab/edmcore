5a33b3e7e3d8d051b49e25a4f847d8a24931e768
James Balamuta <balamut2@illinois.edu>
2020-11-15 18:56:18 -0600

    Condense permutations and regenerate documentation

--- R/permutations.R
+++ R/permutations.R
@@ -61,6 +61,69 @@ permutate_attribute_level_table = function(K, M = 2) {
   level_swap_table
 }
 
+#' Reorganize parameters in Theta Matrix
+#'
+#' @param estimated_theta  Estimated Theta Matrix
+#' @param oracle_theta     Known Theta Matrix
+#' @param K                Number of Attributes
+#'
+#' @return
+#' A vector containing the permutation order
+#' @export
+permutate_theta_order = function(estimated_theta, oracle_theta, k = 3) {
+
+  # Create permutation table
+  permutation_table = gtools::permutations(k, k)
+  n_permutations = nrow(permutation_table)
+
+  # Setup a vector to store results from matching columns
+  distance_by_column = rep(NA, n_permutations)
+
+  # Construct a bijection vector into M = 2
+  vv = attribute_bijection(k)
+
+  # Iterate on trait permutations
+  for (r in seq_len(n_permutations)) {
+    # Retrieve permutation indices
+    col_indices = permutation_table[r,]
+
+    beta_order = c(permuteAtableIndices(
+      nClass = 2 ^ k,
+      k,
+      order = k,
+      vv,
+      col_indices - 1 # remove 1 from indices
+    )) + 1
+
+    # Obtain the mean difference between the current and target matrix
+    distance_by_column[r] = mean(abs(estimated_theta[, beta_order] - oracle_theta))
+  }
+
+  # Obtain the desired column permutation index
+  permutation_idx = which.min(distance_by_column)
+
+  # Determine the best permutation order from 1 to K
+  permutation_order = permutation_table[permutation_idx,]
+
+  beta_order = c(permuteAtableIndices(
+    nClass = 2 ^ k,
+    k,
+    order = k,
+    vv,
+    permutation_order - 1 # remove 1 from indices
+  )) + 1
+
+  # Define a permutation search
+  structure(
+    list(
+      "m" = 2,
+      "k" = k,
+      "permutation_order" = permutation_order,
+      "beta_order" = beta_order
+    ),
+    class = "permutate_order")
+}
+
 
 
 #' Reorder an object to match a target as closely as possible
